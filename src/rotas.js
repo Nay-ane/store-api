9e38d2310e7737fdd6e5710b672a593c0064a9cb
Nay-ane <nay-ane@outlook.com>
2021-10-05 21:27:32 -0300

    teste rota perifl

--- src/rotas.js
+++ src/rotas.js
@@ -7,12 +7,12 @@ const {
 } = require("./controladores/usuarios");
 
 const {
-    cadastrarProdutos, 
-    listarProdutos, 
+    cadastrarProdutos,
+    listarProdutos,
     detalharProduto,
     atualizarProdutos,
-    excluirProdutos
-} = require('./controladores/produtos')
+    excluirProdutos,
+} = require("./controladores/produtos");
 
 const { login } = require("./controladores/login");
 
@@ -23,18 +23,18 @@ const rotas = express();
 //rotas-usuarios
 
 rotas.post("/usuario", cadastrarUsuario);
-rotas.get("/usuario",verificaLogin, detalharUsuario,);
-rotas.put("/usuario",verificaLogin, atualizarUsuario);
+rotas.get("/usuario", verificaLogin, detalharUsuario);
+rotas.get("/perfil", verificaLogin, detalharUsuario);
+rotas.put("/usuario", verificaLogin, atualizarUsuario);
 
 //rota-login
 rotas.post("/login", login);
 
 //rotas-produtos
-rotas.get("/produtos", verificaLogin, listarProdutos)
-rotas.get("/produtos/:id", verificaLogin, detalharProduto)
-rotas.post("/produtos", verificaLogin, cadastrarProdutos)
-rotas.put("/produtos/:id", verificaLogin, atualizarProdutos)
-rotas.delete("/produtos/:id", verificaLogin, excluirProdutos)
-
+rotas.get("/produtos", verificaLogin, listarProdutos);
+rotas.get("/produtos/:id", verificaLogin, detalharProduto);
+rotas.post("/produtos", verificaLogin, cadastrarProdutos);
+rotas.put("/produtos/:id", verificaLogin, atualizarProdutos);
+rotas.delete("/produtos/:id", verificaLogin, excluirProdutos);
 
 module.exports = rotas;
